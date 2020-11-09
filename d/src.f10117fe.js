43501183aa7414c6d6379dcb09b985613b2a2577
Jeanette <jeanette.le@plusgrade.com>
2020-11-09 01:35:40 -0500

    deploying

--- d/src.f10117fe.js
+++ d/src.f10117fe.js
@@ -818,7 +818,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63467" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49951" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
